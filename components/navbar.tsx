99d953ab3c09bd1d58ade2ad345bc147f33f2d23
UnicornDad8 <cecibenitezca@gmail.com>
2026-08-24 13:28:24 -0300

    Add dashboard link to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs";
 import { Button }  from "./ui/button";
-import { KanbanIcon } from "lucide-react";
+import { ArrowRight, KanbanIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Navbar() {
+  const {isSignedIn, user} = useUser();
+
   return (
     <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
       <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
@@ -12,18 +15,29 @@ export default function Navbar() {
           <span className="text-xl sm:text-2xl font-bold text-gray-900">Trello clone</span>
         </div>
         <div className="flex items-center space-x-2 sm:space-x-4">
-          <div>
-            <SignInButton>
-              <Button variant="ghost" size="sm" className="text-xs sm:text-sm">
-                Sign In
-              </Button>
-            </SignInButton>
-            <SignUpButton>
-              <Button size="sm" className="text-xs sm:text-sm">
-                Sign Up
-              </Button>
-            </SignUpButton>
-          </div>
+          {isSignedIn ? 
+            <div className="flex flex-col sm:flex-row items-end sm:items-center space-y-1 sm:space-y-0 sm:space-x-4">
+              <span className="text-xs sm:text-sm text-gray-600 hidden md:block">
+                welcome, {user.firstName ?? user.emailAddresses[0].emailAddress}
+              </span>
+              <Link href="/dashboard">
+                <Button size="sm" className="text-xs sm:text-sm">
+                  Go to dashboard <ArrowRight />
+                </Button>
+              </Link>
+            </div> :  
+            <div>
+              <SignInButton>
+                <Button variant="ghost" size="sm" className="text-xs sm:text-sm">
+                  Sign In
+                </Button>
+              </SignInButton>
+              <SignUpButton>
+                <Button size="sm" className="text-xs sm:text-sm">
+                  Sign Up
+                </Button>
+              </SignUpButton>
+            </div>}
         </div>
       </div>
     </header>
